da32df4a040b388a14818f1e4ca303f74c189d15
egliu <llnatalie@163.com>
2022-11-16 00:34:49 +0800

    实现ffprobe获取视频META信息

--- cmd/transcoding.go
+++ cmd/transcoding.go
@@ -1,1 +1,55 @@
 package cmd
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"github.com/spf13/cobra"
+	"log"
+	"os/exec"
+	"strconv"
+)
+
+var transcodingCmd = &cobra.Command{
+	Use:   "transcoding",
+	Short: "transcoding",
+	Long:  "transcoding",
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("transcoding -- haha")
+		execCmd := exec.Command("ffprobe", "./307478996-1-208.mp4", "-show_streams", "-select_streams", "v", "-print_format", "json")
+		var stdout, stderr bytes.Buffer
+		execCmd.Stdout = &stdout
+		execCmd.Stderr = &stderr
+		err := execCmd.Run()
+		outStr, errStr := string(stdout.Bytes()), string(stderr.Bytes())
+		fmt.Printf("out:\n%s\nerr:\n%s\n", outStr, errStr)
+		if err != nil {
+			log.Fatalf("execCmd.Run() failed with %s\n", err)
+		}
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(transcodingCmd)
+}
+
+type probeFormat struct {
+	Duration string `json:"duration"`
+}
+
+type probeData struct {
+	Format probeFormat `json:"format"`
+}
+
+func probeDuration(a string) (float64, error) {
+	pd := probeData{}
+	err := json.Unmarshal([]byte(a), &pd)
+	if err != nil {
+		return 0, err
+	}
+	f, err := strconv.ParseFloat(pd.Format.Duration, 64)
+	if err != nil {
+		return 0, err
+	}
+	return f, nil
+}
--- go.mod
+++ go.mod
@@ -3,7 +3,11 @@ module transcoding-tool
 go 1.19
 
 require (
+	github.com/aws/aws-sdk-go v1.44.136 // indirect
 	github.com/inconshreveable/mousetrap v1.0.1 // indirect
+	github.com/jmespath/go-jmespath v0.4.0 // indirect
 	github.com/spf13/cobra v1.6.1 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
+	github.com/u2takey/ffmpeg-go v0.4.1 // indirect
+	github.com/u2takey/go-utils v0.3.1 // indirect
 )
